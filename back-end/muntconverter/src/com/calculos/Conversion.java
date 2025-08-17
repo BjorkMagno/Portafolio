896759488546965f05fbc3671bb3dfa222afbb43
BjorkMagno <100161743+BjorkMagno@users.noreply.github.com>
2025-08-17 01:51:48 -0600

    Subiendo conversor de monedad nivel beta

---
+++ back-end/muntconverter/src/com/calculos/Conversion.java
@@ -0,0 +1,38 @@
+package com.calculos;
+
+import com.modelos.Moneda;
+
+public class Conversion {
+    Moneda baseMoneda = new Moneda();
+    Moneda moneda = new Moneda();
+
+    // Contructor
+    public Conversion(Moneda baseMoneda, Moneda moneda) {
+        this.baseMoneda = baseMoneda;
+        this.moneda = moneda;
+    }
+
+    // Metodo
+    public Moneda conversorMoneda (Moneda baseMoneda, Moneda moneda){
+        Moneda newMoneda = new Moneda(baseMoneda.getValor() * moneda.getValor(), moneda.getCode() );
+        return newMoneda;
+    }
+
+    // Getter y Setter
+
+    public Moneda getBaseMoneda() {
+        return baseMoneda;
+    }
+
+    public void setBaseMoneda(Moneda baseMoneda) {
+        this.baseMoneda = baseMoneda;
+    }
+
+    public Moneda getMoneda() {
+        return moneda;
+    }
+
+    public void setMoneda(Moneda moneda) {
+        this.moneda = moneda;
+    }
+}
